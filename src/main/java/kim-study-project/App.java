fcc189c753e3816573a216a342014fa8650da7db
kim-alterpost <kim@alterpost.org>
2019-02-22 18:37:33 +0200

    create new project

---
+++ src/main/java/kim-study-project/App.java
@@ -0,0 +1,13 @@
+package kim-study-project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/kim-study-project/AppTest.java
@@ -0,0 +1,20 @@
+package kim-study-project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
