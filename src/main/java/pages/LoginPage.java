3bb3018f10c50c48b1ec81eb918e2385b0d60cb8
kim-alterpost <kim@alterpost.org>
2019-02-28 10:06:49 +0200

    add ActionsWithElements and Log4J + refactor few mwthods

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,12 +2,24 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage {
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
+
+    @FindBy(name = "email")
+    private WebElement email;
+
+    @FindBy(name = "password")
+    private WebElement password;
+
+    @FindBy(xpath = ".//div[@class='btn-box']//button[@type ='submit']")
+    private WebElement loginButton;
+
 
     public void enterTextInToEmailField(String email) {
         try {
