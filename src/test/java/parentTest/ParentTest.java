fe4d5d866b54dd73893f4a10375c0451a7e2af96
kim-alterpost <kim@alterpost.org>
2019-05-15 15:40:56 +0300

    add-selenoid

--- src/test/java/parentTest/ParentTest.java
+++ src/test/java/parentTest/ParentTest.java
@@ -6,11 +6,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.MyProfilePage;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class ParentTest {
@@ -20,12 +24,18 @@ public class ParentTest {
     protected HomePage homePage;
     protected MyProfilePage myProfilePage;
     Logger logger;
+    String browser = System.getProperty("browser");
 
     @Before
     public void SetUp() {
-        File file = new File("./src/drivers/chromedriver");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        webDriver = new ChromeDriver();
+        logger = Logger.getLogger(getClass());
+
+        setBrowser();
+
+//        File file = new File("./src/drivers/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+//        webDriver = new ChromeDriver();
+
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
@@ -40,6 +50,53 @@ public class ParentTest {
     }
     protected void checkExpectedResult(String message, boolean actualResult) {
         Assert.assertEquals(message, actualResult);
+    }
+
+
+    private void setBrowser() {
+        if (browser == null) {
+            browser = "chrome";
+        }
+        if ("local-chrome".equals(browser)) {
+            File fileFF = new File("./src/drivers/chromedriver");
+            System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+            webDriver = new ChromeDriver();
+        } else if ("chrome".equals(browser)) {
+            logger.info("Remote Chrome Driver will be started");
+            try {
+                DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+                desiredCapabilities.setCapability("enableVNC", true);
+                webDriver = new RemoteWebDriver(
+                        new URL("http://selenoid.pdgamedev.com:4444/wd/hub"),
+                        desiredCapabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else if ("firefox".equals(browser)) {
+            logger.info("Remote Firefox Driver will be started");
+            try {
+                DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+                desiredCapabilities.setCapability("enableVNC", true);
+                webDriver = new RemoteWebDriver(
+                        new URL("http://selenoid.pdgamedev.com:4444/wd/hub"),
+                        desiredCapabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else if ("opera".equals(browser)) {
+            logger.info("Remote Opera Driver will be started");
+            try {
+                DesiredCapabilities desiredCapabilities = DesiredCapabilities.opera();
+                desiredCapabilities.setCapability("enableVNC", true);
+                webDriver = new RemoteWebDriver(
+                        new URL("http://selenoid.pdgamedev.com:4444/wd/hub"),
+                        desiredCapabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Assert.fail("Can not open browser" + browser);
+        }
     }
 
 
